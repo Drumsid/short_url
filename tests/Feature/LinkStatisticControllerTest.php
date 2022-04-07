3d765dd053ece3128c07bddb4f22012efd9f8705
denis.soloduhin <denis.soloduhin@simbirsoft.com>
2022-04-07 18:10:20 +0300

    add link statistic test

--- tests/Feature/LinkStatisticControllerTest.php
+++ tests/Feature/LinkStatisticControllerTest.php
@@ -1,41 +1,31 @@
 <?php
 
-//namespace Tests\Feature;
-//
-//use App\Http\Controllers\LinkStatisticController;
-//use App\Models\Link;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Illuminate\Foundation\Testing\WithFaker;
-//use Illuminate\Http\Request;
-//use Tests\TestCase;
-//
-//class LinkStatisticControllerTest extends TestCase
-//{
-//    private $link;
-//
-//    protected function setUp(): void
-//    {
-//        parent::setUp();
-//        $this->link = Link::factory()->create();
-//    }
-//
-//    public function testGetStatistic()
-//    {
-//        $test = New LinkStatisticController();
-//        dd($test->getStatistic($this->get("/"), $this->link));
-        // 1 пример
-//        $stub = $this->createMock(Request::class);
-//        $stub->method("server")->willReturn("test");
-//        // dd($stub->server());
-//        $this->assertSame('test', $stub->server());
-//
-//        //  2 пример
-//        // пример стаба Link
-//        $stub = $this->createMock(Link::class);
-//        // метод должен реально существовать
-//        $stub->method('sort')
-//            ->willReturn('foo');
-//        // Вызов $stub->sort() теперь вернёт 'foo'
-//        $this->assertSame('foo', $stub->sort());
-//    }
-//}
+namespace Tests\Feature;
+
+use App\Http\Controllers\LinkStatisticController;
+use App\Models\Link;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class LinkStatisticControllerTest extends TestCase
+{
+    private Link $link;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->link = Link::factory()->create();
+    }
+
+    public function testGetStatistic() : void
+    {
+        $statistic = New LinkStatisticController();
+        $request = new Request();
+        $statisticData = $statistic->getStatistic($request, $this->link);
+        $this->assertIsArray($statisticData);
+        $this->assertArrayHasKey("link_id", $statisticData);
+        $this->assertEquals(3, count($statisticData));
+    }
+}
